d2656e8e729cf9dacb8ca81268f66b899686f0a0
dave long <davelongdev@gmail.com>
2024-09-03 11:42:40 -0400

    add documentation coments to main.go

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
+
+  // create mux (an http request multiplexer) to handle incoming requests
+  // and redirect the requests to the correct handler function by 
+  // pattern matching
   mux := http.NewServeMux()
+
+  // create handler function for requests to "/"
   mux.HandleFunc("/", handleRoot)
 
+  // start server and print start message
   fmt.Println("Server listening to :8080")
   http.ListenAndServe(":8080", mux)
 }
@@ -20,6 +27,7 @@ func handleRoot(
 
   // contains http request
   r *http.Request,
+
 ) {
   fmt.Fprintf(w, "Hello World")
 }
